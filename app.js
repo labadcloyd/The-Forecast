efb80eb0b11218ec34e5529f41695c3b848070c9
Cloyd Abad <67862525+labadcloyd@users.noreply.github.com>
2021-01-24 03:10:19 +0000

    The lon and lat converter finally works

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const https = require('https');
+const { static, response } = require('express');
+require('dotenv').config();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'))
+
+app.listen(3000, ()=>{
+  console.log("server is running on port 3000")
+})
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname+'/index.html')
+})
+
+app.post('/', (req,res)=>{
+  
+  let apiKeyLocationIQ = process.env.API_KEY_LOCATIONIQ;
+  let cityLocationIQ = req.body.citySearch;
+  let urlLocationIQ = "https://api.locationiq.com/v1/search.php?format=JSON&key="+apiKeyLocationIQ+"&q="+cityLocationIQ+"&limit=1";
+  https.get(urlLocationIQ, function(response){
+    console.log(response.statusCode)
+    response.on("data",function(data){
+      let locationIQ = JSON.parse(data);
+      var lat= locationIQ[0].lat;
+      var lon= locationIQ[0].lon;
+      console.log(lat, lon)
+    })
+  })
+
+  let urlWeather = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+lat+'&lon='+lon+'&exclude=alerts,minutely&units=metric&appid='+API_KEY_WEATHER;
+  https.get(urlWeather, function(response){
+    
+  })
+  
+})
+
 function timeConverter(UNIX_timestamp){
     let a = new Date(UNIX_timestamp * 1000);
     let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
