24414290fd4658cdca422ddd901ea8f70db04d83
labadcloyd <abadcloyd@gmail.com>
2021-03-28 02:10:19 +0000

    styled the homepage

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar(){
                     <Image src={"/../public/site-logo.png"} alt="" className={"site-logo"}  width={'35px'} height={'35px'}/>
                     <div class={"site-title main"}>The Forecast</div> 
                 </a>
-                <div className={"searchContainer"}>
+                <div className={"search-container"}>
                     <form action={locationUrl} method="POST" id="search-form">
                         <input name="citySearch" className={"searchbar"} placeholder="Enter The Location" onChange={handleChange} value={inputValue}/>
                         <button type="submit" className={"search"}><span className={"material-icons"}>search</span></button>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
+import Image from 'next/image'
 import { useState } from 'react';
-import background from '../public/homeWallpaper.jpg';
 
 export default function Home() {
   const [inputValue, setInput] = useState('')
@@ -9,12 +9,18 @@ export default function Home() {
     setInput(event.target.value)
   }
   return (
-    <div style={{backgroundImage: `url("${background}")`, backgroundColor:'black', height: '100vh'}}>
-      <form method='POST' action={locationUrl}>
-        <label htmlFor='location'></label>
-        <input type='text' name='location' value={inputValue} onChange={handleChange}/>
-        <input type='submit'/>
-      </form>
+    <div className={'home'} style={{display:'flex',justifyContent:'center', alignItems:'center', flexDirection:'column', gap:'3rem'}}>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+      <a href="/" className="title" style={{marginTop:'-60px'}}>
+          <Image src={"/../public/site-logo.png"} alt="" className={"site-logo"}  width={'70px'} height={'70px'}/>
+          <div class={"site-title main"} style={{color:'white', fontSize:'3rem'}}>The Forecast</div> 
+      </a>
+      <div className={"search-container"}>
+        <form method='POST' action={locationUrl} style={{display:'flex', flexDirection:'row'}}>
+          <input type='text' className={"searchbar"} name='location' value={inputValue} onChange={handleChange} style={{backgroundColor:'rgba(0,0,0,0.2)', backdropFilter:'blur(10px)', fontSize:'1.4rem', color:'white'}} placeholder='Enter Location'/>
+          <button type='submit' className={"search"} style={{backgroundColor:'rgba(0,0,0,0.2)', backdropFilter:'blur(10px)'}}><span className={"material-icons"} style={{color:'white'}}>search</span></button>
+        </form>
+      </div>
     </div>
   )
 }
